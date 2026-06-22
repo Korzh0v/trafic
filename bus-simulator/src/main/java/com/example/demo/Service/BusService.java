a06ae2ae96ef156c0c8e2141ab7aaad45090228d
Korzh0v <mkkorzhovsky@gmail.com>
2026-06-22 14:53:28 +0200

    b

--- bus-simulator/src/main/java/com/example/demo/Service/BusService.java
+++ bus-simulator/src/main/java/com/example/demo/Service/BusService.java
@@ -14,35 +14,39 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class BusService {
 
-    private Bus bus = new Bus(1, new BusRoute());
-
     @Autowired
     private KafkaTemplate<String, Bus> template;
+
+    Bus bus = new Bus(1, getRoute());
+
     @Autowired
     private RestTemplate restTemplate;
 
     @Scheduled(fixedRate = 500)
     public void sendMessage() {
-        bus.moveVehicle();
+        RouteResponse route = bus.getRoute();
+        double currentPosition = bus.getLat() / bus.getLng();
+        route.waypoints().removeFirst();
+
         ProducerRecord<String, Bus> record = new ProducerRecord<>(
-                "bus-telemetry",
-                String.valueOf(bus.getNumber()), // ключ = номер автобуса
-                bus
+            "bus-telemetry",
+            String.valueOf(bus.getNumber()), // ключ = номер автобуса
+            bus
         );
         template.send(record);
     }
 
-    public void getRoute () {
+    public RouteResponse getRoute() {
         ResponseEntity<RouteResponse> response = restTemplate.exchange(
-                "https://router.project-osrm.org/route/v1/driving/" +
-                        "30.467663,50.450837;30.524998,50.450181" +
-                        "?steps=true&geometries=geojson",
-                HttpMethod.GET,
-                null,
-                RouteResponse.class
+            "https://router.project-osrm.org/route/v1/driving/" +
+                "30.467663,50.450837;30.524998,50.450181" +
+                "?steps=true&geometries=geojson",
+            HttpMethod.GET,
+            null,
+            RouteResponse.class
         );
 
         RouteResponse body = response.getBody();
-
+        return body;
     }
 }
--- bus-simulator/src/main/java/com/example/demo/entities/RouteResponse.java
+++ bus-simulator/src/main/java/com/example/demo/entities/RouteResponse.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record RouteResponse(
-        String code,
-        List<Waypoint> waypoints,
-        List<Route> routes
-) {
-}
+    String code,
+    List<Waypoint> waypoints,
+    List<Route> routes
+) {}
