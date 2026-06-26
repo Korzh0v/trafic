e38e1d89f106186846863b05789a5306d083328f
Korzh0v <mkkorzhovsky@gmail.com>
2026-06-26 15:04:04 +0200

    stops

--- alert-service/src/main/java/com/example/demo/Service/AlertsService.java
+++ alert-service/src/main/java/com/example/demo/Service/AlertsService.java
@@ -18,7 +18,7 @@ public class AlertsService {
     public ResponseEntity<BusEventDTO> printEvent(String message) {
         BusEventDTO event = mapper.readValue(message, BusEventDTO.class);
         System.out.println(event);
-
+        System.out.println("1");
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
             event
         );
